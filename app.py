da3da1f59df2250944b96d1df2564e972e11955d
Emmanuel <zabayo92@gmail.com>
2023-06-03 15:11:04 +0200

    configuring keys

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ gee_data = get_fused_data()
 
 def get_features(longitude, latitude):
     # TODO: Create an ee.Geometry instance from the coordinates
-    points = ee.Geometry.Points(longitude,latitude)
+    points = ee.Geometry.Point(longitude,latitude)
 
     # TODO: Sample features for the given point of interest keeping only the training bands
     dataclean = gee_data.select(trainingbands).sampleRegions(collection=points,
